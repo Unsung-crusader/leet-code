1cfe3d1eff9af456411963ed6ccd9c7fa343035f
Prateek Thapa <prateekthapa26@gmail.com>
2019-05-12 10:10:46 +0545

    two sum

---
+++ README.md
@@ -0,0 +1,6 @@
+# leet-code
+Solving coding challenges from leetcode.com
+
+Link to profile
+
+https://leetcode.com/unsung-crusader/
---
+++ twoSum.js
@@ -0,0 +1,35 @@
+/*** ===================================================================== ***\
+ ** - PROBLEM ------------------------------------------------------ **
+ * ========================================================================= *
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+        return [0, 1].
+
+\*** ===================================================================== ***/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && i !== j) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
+console.log(twoSum([3, 2, 4], 6)); // [1,2]
+console.log(twoSum([3, 3], 6));
+console.log(twoSum([2, 5, 5, 11], 10)); // [1,2]

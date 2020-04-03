f9fe3cb988e778bb9e4d4e81915a157645e235b2
Prateek Thapa <prateekthapa26@gmail.com>
2020-04-03 20:40:31 +0545

    maxSubArray with two-pass

---
+++ 30-day-leetcode-challenge/3.maxSubArray.js
@@ -0,0 +1,26 @@
+/**
+  Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ */
+
+function maxSubArray(nums) {
+  if (nums.length === 1) return nums[0];
+
+  let maxSum = -Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    let prev = nums[i];
+    maxSum = Math.max(prev, maxSum);
+    
+    for (let j = i; j < nums.length - 1; j++) {
+      prev += nums[j + 1];
+      maxSum = Math.max(prev, maxSum);
+    }
+  }
+
+  return maxSum;
+}
+
+// console.log(maxSubArray([1, 2, 3]))
+// console.log(maxSubArray([-2, 1]));
+console.log(maxSubArray([-1, -2]));
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
